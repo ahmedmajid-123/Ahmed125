0940706ae0caa919835442accc9334f2c860fa14
ahmedmajid-123 <ahmedmajid12347@gmail.com>
2025-08-01 15:41:04 +0500

    apna cmmit

---
+++ ahmed.js
@@ -0,0 +1,11 @@
+console.log("Ahmed Majid")
+let array = []
+console.log(array)
+let res1 = array.shift("Ahmed")
+console.log(res1)
+let res2 = array.push("Shoaib")
+console.log(res2)
+let res3 = array.indexOf(20)
+console.log(res3)
+
+
